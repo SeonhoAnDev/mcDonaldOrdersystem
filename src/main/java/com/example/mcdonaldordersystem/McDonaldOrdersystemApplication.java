f87ae84feea3c3b0ed7e58e0cdb90af18c7e8525
sonho_an <seonhoan022@gmail.com>
2024-12-30 18:22:57 +0900

    orderSystem
    プロジェクト作成

---
+++ src/main/java/com/example/mcdonaldordersystem/McDonaldOrdersystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.mcdonaldordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McDonaldOrdersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McDonaldOrdersystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mcdonaldordersystem/McDonaldOrdersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mcdonaldordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McDonaldOrdersystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
